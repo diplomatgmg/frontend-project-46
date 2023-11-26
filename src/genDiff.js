614a3c66a2e1234b4f3a65226d5e0e63dd0f847c
Кирилл Попов <diplomatgmg@gmail.com>
2023-11-26 22:34:25 +0300

    Добавлен парсинг файлов

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,1 +1,8 @@
-export default (filepath1, filepath2) => [filepath1, filepath2];
+import parseFile from './parseFile.js';
+
+export default (filePath1, filePath2) => {
+  const fileData1 = parseFile(filePath1);
+  const fileData2 = parseFile(filePath2);
+
+  return [fileData1, fileData2];
+};
