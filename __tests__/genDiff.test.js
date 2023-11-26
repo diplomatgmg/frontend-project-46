8a4f09b2787acce990cc44748e5cf90f96315df6
Кирилл Попов <diplomatgmg@gmail.com>
2023-11-26 22:34:37 +0300

    Добавлены тесты

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,5 +1,24 @@
+import path from 'path';
 import genDiff from '../index.js';
 
-test('genDiff', () => {
-  expect(genDiff('example1.txt', 'example2.txt')).toEqual(['example1.txt', 'example2.txt']);
+const fixturesDir = path.join(__dirname, '..', '__fixtures__');
+
+test('read files test', () => {
+  const filePath1 = path.join(fixturesDir, 'file1.json');
+  const filePath2 = path.join(fixturesDir, 'file2.json');
+
+  const expectData1 = {
+    host: 'hexlet.io',
+    timeout: 50,
+    proxy: '123.234.53.22',
+    follow: false,
+  };
+
+  const expectData2 = {
+    timeout: 20,
+    verbose: true,
+    host: 'hexlet.io',
+  };
+
+  expect(genDiff(filePath1, filePath2)).toEqual([expectData1, expectData2]);
 });
