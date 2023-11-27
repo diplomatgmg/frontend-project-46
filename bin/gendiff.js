e519773e5cd9d84bce06896e5bdc9c51d804e13a
Кирилл Попов <diplomatgmg@gmail.com>
2023-11-27 20:40:00 +0300

    Добавлено сравнение вложенных JSON

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .name('gendiff')
   .description('  Compares two configuration files and shows a difference.')
-  .version('1.2.0')
+  .version('1.3.0')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
