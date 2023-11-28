bcc979431da09690649ba37e65c381d938eec68d
Кирилл Попов <diplomatgmg@gmail.com>
2023-11-28 03:58:30 +0300

    Улучшена читаемость кода
    Ожидаемые результаты вынесены в отдельный файл

--- __tests__/genDiff.test.ts
+++ __tests__/genDiff.test.ts
@@ -1,111 +1,67 @@
 import path from 'path'
 import genDiff from '../index'
+import fs from 'fs'
 
 const fixturesDir = path.join(__dirname, '..', '__fixtures__')
 
-describe('diff stylish files test', () => {
-  const expectedData = `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`
+function loadFixture (fileName: string): string {
+  return fs.readFileSync(path.join(fixturesDir, fileName), 'utf-8').trim()
+}
 
-  test('diff json', () => {
-    const filePath1 = path.join(fixturesDir, 'file1.json')
-    const filePath2 = path.join(fixturesDir, 'file2.json')
+function getFilePath (fileName: string): string {
+  return path.join(fixturesDir, fileName)
+}
+
+describe('Diff stylish files test', () => {
+  const expectedData = loadFixture('expectedStylish')
+
+  test('Diff JSON', () => {
+    const filePath1 = getFilePath('file1.json')
+    const filePath2 = getFilePath('file2.json')
 
     expect(genDiff(filePath1, filePath2)).toEqual(expectedData)
   })
 
-  test('diff yaml', () => {
-    const filePath1 = path.join(fixturesDir, 'file1.yaml')
-    const filePath2 = path.join(fixturesDir, 'file2.yaml')
+  test('Diff YAML', () => {
+    const filePath1 = getFilePath('file1.yaml')
+    const filePath2 = getFilePath('file2.yaml')
 
     expect(genDiff(filePath1, filePath2)).toEqual(expectedData)
   })
 })
 
-describe('diff plain files test', () => {
-  const expectedData = `Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]`
+describe('Diff plain files test', () => {
+  const expectedData = loadFixture('expectedPlain')
 
-  test('diff stylish json', () => {
-    const filePath1 = path.join(fixturesDir, 'file1.json')
-    const filePath2 = path.join(fixturesDir, 'file2.json')
+  test('Diff stylish JSON', () => {
+    const filePath1 = getFilePath('file1.json')
+    const filePath2 = getFilePath('file2.json')
 
     expect(genDiff(filePath1, filePath2, 'plain')).toEqual(expectedData)
   })
 
-  test('diff stylish yaml', () => {
-    const filePath1 = path.join(fixturesDir, 'file1.yaml')
-    const filePath2 = path.join(fixturesDir, 'file2.yaml')
+  test('Diff stylish YAML', () => {
+    const filePath1 = getFilePath('file1.yaml')
+    const filePath2 = getFilePath('file2.yaml')
 
     expect(genDiff(filePath1, filePath2, 'plain')).toEqual(expectedData)
   })
 })
 
-describe('diff json format files test', () => {
-  const expectedData = '{"common":{"type":"nested","children":{"follow":{"value":false,"type":"added"},"setting1":{"value":"Value 1","type":"unchanged"},"setting2":{"value":200,"type":"removed"},"setting3":{"value":null,"oldValue":true,"type":"changed"},"setting4":{"value":"blah blah","type":"added"},"setting5":{"value":{"key5":"value5"},"type":"added"},"setting6":{"type":"nested","children":{"doge":{"type":"nested","children":{"wow":{"value":"so much","oldValue":"","type":"changed"}}},"key":{"value":"value","type":"unchanged"},"ops":{"value":"vops","type":"added"}}}}},"group1":{"type":"nested","children":{"baz":{"value":"bars","oldValue":"bas","type":"changed"},"foo":{"value":"bar","type":"unchanged"},"nest":{"value":"str","oldValue":{"key":"value"},"type":"changed"}}},"group2":{"value":{"abc":12345,"deep":{"id":45}},"type":"removed"},"group3":{"value":{"deep":{"id":{"number":45}},"fee":100500},"type":"added"}}'
+describe('Diff JSON format files test', () => {
+  const expectedData = loadFixture('expectedJson')
 
-  test('diff json json', () => {
-    const filePath1 = path.join(fixturesDir, 'file1.json')
-    const filePath2 = path.join(fixturesDir, 'file2.json')
+  test('Diff JSON JSON', () => {
+    const filePath1 = getFilePath('file1.json')
+    const filePath2 = getFilePath('file2.json')
 
     expect(genDiff(filePath1, filePath2, 'json')).toEqual(expectedData)
   })
 })
 
-test('unsupported file extension', () => {
-  const filePath1 = path.join(fixturesDir, 'unsupported.txt')
-  const filePath2 = path.join(fixturesDir, 'unsupported.txt')
+test('Unsupported file extension', () => {
+  const filePath1 = getFilePath('unsupported.txt')
+  const filePath2 = getFilePath('unsupported.txt')
 
   expect(() => genDiff(filePath1, filePath2)).toThrow()
 })
